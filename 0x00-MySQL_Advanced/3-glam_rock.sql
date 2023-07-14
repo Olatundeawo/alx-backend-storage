11a7635331b4c2357590e7ebc8bdb414d38fd1e9
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-07-14 12:10:37 -0500

    added task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,8 @@
+-- A script that lists bands with
+-- a particular style and ranked
+-- based on their longevity
+
+SELECT band_name AS band_name,IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
